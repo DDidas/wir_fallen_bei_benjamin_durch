1288539afda54b45ccf15c7585ae2b8d6d5ed169
Lucca <luccaglessner@gmx.de>
2023-07-24 18:58:51 +0200

    Ganze Zahlen und Multiplikativ Inverses

---
+++ pythonScriptsReadyForTheKlausur/GanzeZahlen_S_und_T_mit_EuklidischemAlgorithmus.py
@@ -0,0 +1,15 @@
+def erweiterter_euklidischer_algorithmus(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        ggt, x, y = erweiterter_euklidischer_algorithmus(b % a, a)
+        return ggt, y - (b // a) * x, x
+
+def main():
+    a = int(input("Geben Sie den ersten Wert ein: "))
+    b = int(input("Geben Sie den zweiten Wert ein: "))
+    ggt, s, t = erweiterter_euklidischer_algorithmus(a, b)
+    print(f"ggT({a}, {b}) = {ggt} = {s}*{a} + {t}*{b}")
+
+if __name__ == "__main__":
+    main()
