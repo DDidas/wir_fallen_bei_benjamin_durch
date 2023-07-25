f51c4b1485252eadedfb97e54a7f777010cbebed
DDidas <david.didas@aol.com>
2023-07-25 19:27:30 +0200

    ws

--- Belegaufgaben/Beleg 6/Warteschlange(Anpassungstest)_Beleg6.py
+++ Belegaufgaben/Beleg 6/Warteschlange(Anpassungstest)_Beleg6.py
@@ -1,6 +1,9 @@
 import numpy as np
 import scipy.stats as stats
-import re
+import pandas as pd
+
+# Diese Anweisung weist Pandas an, alle Spalten anzuzeigen
+pd.set_option('display.max_columns', None)
 
 # Eingabe der Daten
 n = int(input("Bitte geben Sie die Gesamtzahl der Beobachtungen ein: "))
@@ -10,33 +13,42 @@ mean_delta_t = float(input("Bitte geben Sie den Mittelwert ein: "))
 observed_freqs = np.array([int(i) for i in input("Bitte geben Sie die beobachteten Häufigkeiten ein, getrennt durch '&': ").split('&')])
 
 # Eingabe der oberen Intervallgrenzen
-upper_bounds_input = input("Bitte geben Sie die oberen Intervallgrenzen ein, getrennt durch '&', endend mit 'inf': ")
+intervals = [int(i) if i != 'inf' else np.inf for i in input("Bitte geben Sie die oberen Intervallgrenzen ein, getrennt durch '&', endend mit 'inf': ").split('&')]
 
-# Extrahieren der Zahlen aus der Eingabe
-upper_bounds = [float(i) if i != 'inf' else np.inf for i in re.findall(r'[\d\.inf]+', upper_bounds_input)]
+# Berechnung der theoretischen Wahrscheinlichkeiten und Häufigkeiten
+lambda_hat = 1 / mean_delta_t  # Schätzwert für Lambda
+lower_bounds = [0] + intervals[:-1]
+pi_theo = np.array([np.exp(-lambda_hat * lower_bounds[i]) - np.exp(-lambda_hat * intervals[i]) for i in range(len(intervals))])
+ni_star_theo = n * pi_theo
 
-# Erzeugung der Intervallgrenzen
-intervals = [0] + upper_bounds
+# Erstellung der Tabelle
+df = pd.DataFrame()
+df['Klasse'] = [f"[{lower_bounds[i]},{intervals[i]})" for i in range(len(intervals))]
+df['n_i (Stichprobe)'] = observed_freqs
+df['p_i (theoretisch)'] = pi_theo
+df['n_i* (theoretisch)'] = ni_star_theo
+df['(n_i - n_i*)^2 / n_i*'] = np.power(observed_freqs - ni_star_theo, 2) / ni_star_theo
 
-# Berechnung der theoretischen Wahrscheinlichkeiten für jedes Intervall
-lambda_hat = 1 / mean_delta_t
-theoretical_probs = [stats.expon.cdf(intervals[i+1], scale=1/lambda_hat) - stats.expon.cdf(intervals[i], scale=1/lambda_hat) for i in range(len(intervals)-1)]
+# Hinzufügen der Summenzeile
+sum_row = pd.DataFrame(df.sum(numeric_only=True)).transpose()
+sum_row["Klasse"] = "Σ"
+df = pd.concat([df, sum_row], ignore_index=True)
 
-# Berechnung der erwarteten Häufigkeiten
-expected_freqs = n * np.array(theoretical_probs)
+print(df)
 
-# Berechnung der Teststatistik
-sq_diffs = (observed_freqs - expected_freqs)**2 / expected_freqs
-test_statistic = np.sum(sq_diffs)
+# Berechnung des Testwerts
+test_value = np.sum(np.power(observed_freqs - ni_star_theo, 2) / ni_star_theo)
 
-# Vergleich der Teststatistik mit der kritischen Grenze
-critical_value = stats.chi2.ppf(0.99, df=len(observed_freqs)-2)
+# Berechnung der Freiheitsgrade und der kritischen Grenze
+df_ = len(observed_freqs) - 2  # Freiheitsgrade (Anzahl der Intervalle - 1 - Anzahl der geschätzten Parameter)
+critical_value = stats.chi2.ppf(0.95, df_)  # Kritischer Wert bei einem Signifikanzniveau von 5%
 
-print(f"\nSchätzwert \hat\lambda = {lambda_hat}\n")
-print(f"Testwert = {test_statistic}\n")
-print(f"Kritische Grenze = {critical_value}\n")
+# Ausgabe der Ergebnisse
+print(f"\nSchätzwert \hat\lambda = {lambda_hat}")
+print(f"\nTestwert = {test_value}")
+print(f"\nKritische Grenze = {critical_value}")
 
-if test_statistic <= critical_value:
-    print("Wir können davon ausgehen, dass die vorliegenden Ankunftsabstände exponentialverteilt sind.")
+if test_value <= critical_value:
+    print("\nWir können davon ausgehen, dass die vorliegenden Ankunftsabstände exponentialverteilt sind.")
 else:
-    print("Wir können nicht davon ausgehen, dass die vorliegenden Ankunftsabstände exponentialverteilt sind.")
+    print("\nWir können davon NICHT ausgehen, dass die vorliegenden Ankunftsabstände exponentialverteilt sind.")
